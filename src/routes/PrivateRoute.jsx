a5cda5532a480145b6ce99d8a903d40549d3e58e
shoyonp <shoyonkumar143@gmail.com>
2024-12-23 11:59:39 +0600

    all done

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -4,15 +4,15 @@ import { Navigate, useLocation } from "react-router-dom";
 import Loading from "../components/Loading";
 
 const PrivateRoute = ({ children }) => {
-  const { user,loading } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
   const location = useLocation();
-  if(loading){
-    return <Loading></Loading>
+  if (loading) {
+    return <Loading></Loading>;
   }
   if (user && user?.email) {
     return children;
   }
-  return <Navigate state={location.pathname} to={"/auth/login"}></Navigate>;
+  return <Navigate state={location?.pathname} to="/auth/login"></Navigate>;
 };
 
 export default PrivateRoute;
